11b43859e5ecf0bca3354fb6ad1f2c72c38b9673
FJRBach <127007313+FJRBach@users.noreply.github.com>
2024-10-18 20:30:01 -0600

    V1-0-0

---
+++ grafja/src/programaFTLC/principal.java
@@ -0,0 +1,13 @@
+package programaFTLC;
+import java.util.Scanner;
+
+
+public class principal {
+    
+ public static void main(String [] args) {
+  ventanaPrcp principal = new ventanaPrcp();
+  principal.setVisible(true);
+ }
+    
+    
+}
